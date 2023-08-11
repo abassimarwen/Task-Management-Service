481c739382126b6a0f80d152455a39717df5a7c8
abassimarwen <marwen.abassi@esprit.tn>
2023-08-12 00:43:00 +0100

    buissines layer task service

--- src/main/java/com/example/TaskManagementService/domain/mapper/TaskMapper.java
+++ src/main/java/com/example/TaskManagementService/domain/mapper/TaskMapper.java
@@ -4,10 +4,13 @@ import com.example.TaskManagementService.domain.dto.TaskDto;
 import com.example.TaskManagementService.domain.entity.Task;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 
-@Mapper
+@Mapper(componentModel = "spring")
+@Component
 public interface TaskMapper {
     TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
     TaskDto taskEntityToDto(Task task);
     Task taskDtoToEntity(TaskDto taskDto);
+    Task mapTask(Task task,TaskDto taskDto);
 }
